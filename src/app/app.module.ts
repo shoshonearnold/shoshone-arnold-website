ae02641c2e28f93a900e52804fc701a7e31c07f1
shoshone arnold <eastcoastshony@gmail.com>
2023-11-11 12:22:41 -0700

    Revert "Revert "feat(home) added slider""
    This reverts commit 4f5693118318864f27c0bfc69c35072ae8f97f83.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { gsap } from 'gsap';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+
+})
+export class HomeComponent implements OnInit {
+  ngOnInit() {
+    this.svgMagic();
+  }
+
+  svgMagic(): void {
+    gsap.to('#path1', { y: 80, yoyo: true, duration: 12, repeat: -1 });
+    gsap.to('#path2', { y: 65, yoyo: true, duration: 6, repeat: -1 });
+    gsap.to('#path3', { y: 40, yoyo: true, duration: 3, repeat: -1 });
+    gsap.to('#path4', { y: 19, yoyo: true, duration: 6, repeat: -1 });
+    gsap.to('#path5', { y: 22, yoyo: true, duration: 4, repeat: -1 });
+  }
+}
