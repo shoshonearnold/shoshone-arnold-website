1b86625485d5aad83dbab7a85c85b9b2c6547aad
shoshone arnold <eastcoastshony@gmail.com>
2023-11-11 09:19:05 -0700

    Revert "feat(hosting): added hosting with cloudbase"
    This reverts commit 26fd8529f23ed05d22ee5fbf1df8016390e3924e.

--- src/main.ts
+++ src/main.ts
@@ -5,25 +5,3 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
-
-  // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyA3tltRRT0APVa6Ly1JA_96E-GVmlU8Bwg",
-  authDomain: "shoshone-arnolds-website.firebaseapp.com",
-  projectId: "shoshone-arnolds-website",
-  storageBucket: "shoshone-arnolds-website.appspot.com",
-  messagingSenderId: "6853342573",
-  appId: "1:6853342573:web:7c5fb2748fa2afcd6b9664",
-  measurementId: "G-8L98W2CX35"
-};
-
-// Initialize Firebase
-initializeApp(firebaseConfig);
-//getAnalytics(app);
