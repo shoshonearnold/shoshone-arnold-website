04c8a513615b6c7bcce2320d4bd0ebdddd658151
Shoshone Arnold <shoshonearnold@Shoshones-Mac-Studio.local>
2023-12-29 12:59:10 -0700

    updated nav

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {}
+export class NavbarComponent implements OnInit {
+  ngOnInit(): void {
+    /*
+  Slidemenu
+*/
+    (function () {
+      var $body = document.body,
+        $menu_trigger = $body.getElementsByClassName('menu-trigger')[0];
+
+      if (typeof $menu_trigger !== 'undefined') {
+        $menu_trigger.addEventListener('click', function () {
+          $body.className =
+            $body.className == 'menu-active' ? '' : 'menu-active';
+        });
+      }
+    }).call(this);
+  }
+}
