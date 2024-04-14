bc506500bbfb47169ba360fb64e496a98863a9e4
Shoshone Arnold <42348679+shoshonearnold@users.noreply.github.com>
2024-04-14 15:11:33 -0600

    implemented routing (#8)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { provideRouter } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BlogComponent } from './blog/blog.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'about-me', component: AboutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [provideRouter(routes)],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
