1fff6300fb9dcbd9025d1050f36924ec84cd1383
Shoshone Arnold <42348679+shoshonearnold@users.noreply.github.com>
2024-04-15 17:41:16 -0600

    WIP: bc50650 implemented routing (#8) (#9)

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Links } from './enums/links.enum';
 
 @Component({
@@ -6,24 +6,8 @@ import { Links } from './enums/links.enum';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
   LINKEDIN = Links.LINKEDIN;
   GITHUB = Links.GITHUB;
   EMAIL = Links.EMAIL;
-  ngOnInit(): void {
-    this.controlMenu();
-  }
-
-  controlMenu(): void {
-    (() => {
-      const body = document.body;
-      const menuInit = body.getElementsByClassName('menu-trigger')[0];
-
-      if (typeof menuInit !== 'undefined') {
-        menuInit.addEventListener('click', () => {
-          body.className === 'menu-active' ? '' : 'menu-active';
-        });
-      }
-    }).call(this);
-  }
 }
